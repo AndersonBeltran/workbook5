99bbbf34fc2c87004444d8a745d8b0e47e7ba5d4
AndersonBeltran <abeltran@appdev.yearup.org>
2024-10-28 10:25:39 -0400

    find first

--- demos/find-first.js
+++ demos/find-first.js
@@ -1,2 +1,11 @@
 "use strict";
 let numbers = [7, 9, 64, 60, 12, 13, 65, 62];
+function isOver60(number) {
+  if (number > 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let over60 = numbers.find(isOver60);
+console.log(over60);
